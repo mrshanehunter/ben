984d955278480ee5db83d29c4633ab87deeb8bbd
mrshanehunter <webdev@thatguyfrommarketing.com>
2022-02-13 15:43:46 +1100

    add color

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <ContentContainer>
-      <Fade cascade className="engrave">
+      <Fade cascade delay={3000} className="engrave">
         <h1>B</h1>
         <h1>E</h1>
         <h1>N</h1>
